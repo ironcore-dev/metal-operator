46116de2628d2e6324c4b9c32b53a313c7ec38f0
Stefan Hipfel <stefan.hipfel@sap.com>
2025-03-26 17:09:39 +0100

    updates server reconciliation logic to handle ServerClaimRef state transitions

--- internal/controller/server_controller.go
+++ internal/controller/server_controller.go
@@ -173,6 +173,12 @@ func (r *ServerReconciler) reconcile(ctx context.Context, log logr.Logger, serve
 		}
 	}
 
+	if server.Spec.ServerClaimRef != nil {
+		if modified, err := r.patchServerState(ctx, server, metalv1alpha1.ServerStateReserved); err != nil || modified {
+			return ctrl.Result{}, err
+		}
+	}
+
 	// TODO: This needs be reworked later as the Server cleanup has to happen here. For now we just transition the server
 	// 		 back to available state.
 	if server.Spec.ServerClaimRef == nil && server.Status.State == metalv1alpha1.ServerStateReserved {

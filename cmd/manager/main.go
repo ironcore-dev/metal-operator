69a59cd13aa738654903b0595e79a57ec2872c91
Nagadeesh Nagaraja <nagdeesh@gmail.com>
2025-05-02 08:31:42 +0200

    Add webhook for biosVersion resources

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -401,6 +401,13 @@ func main() { // nolint: gocyclo
 		setupLog.Error(err, "unable to create controller", "controller", "BIOSVersion")
 		os.Exit(1)
 	}
+	// nolint:goconst
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = webhookmetalv1alpha1.SetupBIOSVersionWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "BIOSVersion")
+			os.Exit(1)
+		}
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {

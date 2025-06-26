50589a5dd99d7b1c71cded4b2b359ae750088a81
Nag <nagdeesh@gmail.com>
2025-06-26 11:24:23 +0200

    Update internal/controller/bmc_controller.go
    Co-authored-by: SzymonSAP <szymon.pietraszek@sap.com>

--- internal/controller/bmc_controller.go
+++ internal/controller/bmc_controller.go
@@ -70,7 +70,7 @@ func (r *BMCReconciler) delete(ctx context.Context, log logr.Logger, bmcObj *met
 			return ctrl.Result{}, fmt.Errorf("failed to get BMCSettings for BMC: %w", err)
 		}
 		if err := r.Delete(ctx, bmcSettings); err != nil {
-			return ctrl.Result{}, fmt.Errorf("failed to deleted referred BMCSettings. %w", err)
+			return ctrl.Result{}, fmt.Errorf("failed to delete referred BMCSettings. %w", err)
 		}
 	}
 

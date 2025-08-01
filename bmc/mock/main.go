b128a591b4a681f2748fab450d34dade2c67c8d2
Andreas Fritzler <andreas.fritzler@sap.com>
2025-08-01 14:25:20 +0200

    Add pure go Redfish mock server implementation

---
+++ bmc/mock/main.go
@@ -0,0 +1,41 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and IronCore contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	"github.com/ironcore-dev/metal-operator/bmc/mock/server"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	opts := zap.Options{
+		Development: true,
+	}
+
+	log := ctrl.Log.WithName("setup")
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+
+	srv := server.NewMockServer(log, ":8000")
+
+	if err := srv.Start(ctx); err != nil {
+		log.Error(err, "Failed to start mock server")
+		return
+	}
+
+	<-ctx.Done()
+
+	time.Sleep(1 * time.Second)
+	log.Info("Mock server stopped")
+}

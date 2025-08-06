e1ecd170fb6f13971351f585c09ab1b5b202c09a
Nagadeesh Nagaraja <nagdeesh@gmail.com>
2025-08-06 16:59:29 +0200

    fix printColumn

--- api/v1alpha1/biossettingsset_types.go
+++ api/v1alpha1/biossettingsset_types.go
@@ -37,7 +37,7 @@ type BIOSSettingsSetStatus struct {
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:scope=Cluster
-// +kubebuilder:printcolumn:name="BIOSVersion",type=string,JSONPath=`.spec.version`
+// +kubebuilder:printcolumn:name="BIOSVersion",type=string,JSONPath=`.spec.biosSettingsTemplate.version`
 // +kubebuilder:printcolumn:name="TotalServers",type="string",JSONPath=`.status.fullyLabeledServers`
 // +kubebuilder:printcolumn:name="AvailableBIOSSettings",type="string",JSONPath=`.status.availableBIOSSettings`
 // +kubebuilder:printcolumn:name="Pending",type="string",JSONPath=`.status.pendingBIOSSettings`

b5c7e2090c3bffe017443d268a34945988af51ed
Andreas Fritzler <andreas.fritzler@sap.com>
2025-12-18 13:33:26 +0100

    Refactor `BIOSVersion` webhook
    - Refactor `BIOSVersion` webhook
    - Updated tests

--- internal/webhook/v1alpha1/biosversion_webhook_test.go
+++ internal/webhook/v1alpha1/biosversion_webhook_test.go
@@ -4,6 +4,7 @@
 package v1alpha1
 
 import (
+	"github.com/ironcore-dev/metal-operator/internal/controller"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	v1 "k8s.io/api/core/v1"
@@ -24,7 +25,6 @@ var _ = Describe("BIOSVersion Webhook", func() {
 		By("Creating an BIOSVersion")
 		biosVersionV1 = &metalv1alpha1.BIOSVersion{
 			ObjectMeta: metav1.ObjectMeta{
-				Namespace:    "ns.Name",
 				GenerateName: "test-",
 			},
 			Spec: metalv1alpha1.BIOSVersionSpec{
@@ -37,163 +37,158 @@ var _ = Describe("BIOSVersion Webhook", func() {
 			},
 		}
 		Expect(k8sClient.Create(ctx, biosVersionV1)).To(Succeed())
-		SetClient(k8sClient)
-		DeferCleanup(k8sClient.Delete, biosVersionV1)
 	})
 
-	Context("When creating or updating BIOSVersion under Validating Webhook", func() {
-		It("Should deny creation if a Spec.ServerRef field is duplicate", func(ctx SpecContext) {
-			By("Creating another BIOSVersion with existing ServerRef")
-			biosVersionV2 := &metalv1alpha1.BIOSVersion{
-				ObjectMeta: metav1.ObjectMeta{
-					Namespace:    "ns.Name",
-					GenerateName: "test-",
-				},
-				Spec: metalv1alpha1.BIOSVersionSpec{
-					BIOSVersionTemplate: metalv1alpha1.BIOSVersionTemplate{
-						Version:                 "P71 v1.45 (12/06/2017)",
-						Image:                   metalv1alpha1.ImageSpec{URI: "two"},
-						ServerMaintenancePolicy: metalv1alpha1.ServerMaintenancePolicyEnforced,
-					},
-					ServerRef: &v1.LocalObjectReference{Name: "foo"},
-				},
-			}
-			Expect(validator.ValidateCreate(ctx, biosVersionV2)).Error().To(HaveOccurred())
-		})
+	AfterEach(func() {
+		By("Deleting the BIOSVersion and Server resources")
+		Expect(k8sClient.DeleteAllOf(ctx, &metalv1alpha1.BIOSVersion{})).To(Succeed())
 
-		It("Should create if a Spec.ServerRef field is NOT duplicate", func() {
-			By("Creating another BIOSVersion with different ServerRef")
-			biosVersionV2 := &metalv1alpha1.BIOSVersion{
-				ObjectMeta: metav1.ObjectMeta{
-					Namespace:    "ns.Name",
-					GenerateName: "test-",
-				},
-				Spec: metalv1alpha1.BIOSVersionSpec{
-					BIOSVersionTemplate: metalv1alpha1.BIOSVersionTemplate{
-						Version:                 "P71 v1.45 (12/06/2017)",
-						Image:                   metalv1alpha1.ImageSpec{URI: "asd"},
-						ServerMaintenancePolicy: metalv1alpha1.ServerMaintenancePolicyEnforced,
-					},
-					ServerRef: &v1.LocalObjectReference{Name: "bar"},
-				},
-			}
-			Expect(k8sClient.Create(ctx, biosVersionV2)).To(Succeed())
-			DeferCleanup(k8sClient.Delete, biosVersionV2)
-		})
-
-		It("Should deny update if a Spec.ServerRef field is duplicate", func() {
-			By("Creating an BIOSVersion with different ServerRef")
-			biosVersionV2 := &metalv1alpha1.BIOSVersion{
-				ObjectMeta: metav1.ObjectMeta{
-					Namespace:    "ns.Name",
-					GenerateName: "test-",
-				},
-				Spec: metalv1alpha1.BIOSVersionSpec{
-					BIOSVersionTemplate: metalv1alpha1.BIOSVersionTemplate{
-						Version:                 "P71 v1.45 (12/06/2017)",
-						Image:                   metalv1alpha1.ImageSpec{URI: "asd"},
-						ServerMaintenancePolicy: metalv1alpha1.ServerMaintenancePolicyEnforced,
-					},
-					ServerRef: &v1.LocalObjectReference{Name: "bar"},
-				},
-			}
-			Expect(k8sClient.Create(ctx, biosVersionV2)).To(Succeed())
-			DeferCleanup(k8sClient.Delete, biosVersionV2)
-
-			By("Updating an biosVersionV2 to conflicting Spec.ServerRef")
-			biosVersionV2Updated := biosVersionV2.DeepCopy()
-			biosVersionV2Updated.Spec.ServerRef = &v1.LocalObjectReference{Name: "foo"}
-			Expect(validator.ValidateUpdate(ctx, biosVersionV2, biosVersionV2Updated)).Error().To(HaveOccurred())
-		})
-
-		It("Should allow update if a different field is duplicate", func() {
-			By("Creating an BIOSVersion with different ServerRef")
-			biosVersionV2 := &metalv1alpha1.BIOSVersion{
-				ObjectMeta: metav1.ObjectMeta{
-					Namespace:    "ns.Name",
-					GenerateName: "test-",
-				},
-				Spec: metalv1alpha1.BIOSVersionSpec{
-					BIOSVersionTemplate: metalv1alpha1.BIOSVersionTemplate{
-						Version:                 "P71 v1.45 (12/06/2017)",
-						Image:                   metalv1alpha1.ImageSpec{URI: "two"},
-						ServerMaintenancePolicy: metalv1alpha1.ServerMaintenancePolicyEnforced,
-					},
-					ServerRef: &v1.LocalObjectReference{Name: "bar"},
-				},
-			}
-			Expect(k8sClient.Create(ctx, biosVersionV2)).To(Succeed())
-			DeferCleanup(k8sClient.Delete, biosVersionV2)
-
-			By("Updating an biosVersionV2 to conflicting Spec.BIOSVersionSpec")
-			biosVersionV2Updated := biosVersionV2.DeepCopy()
-			biosVersionV2Updated.Spec.Image = biosVersionV1.Spec.Image
-			Expect(validator.ValidateUpdate(ctx, biosVersionV2, biosVersionV2Updated)).Error().ToNot(HaveOccurred())
-		})
-
-		It("Should allow update if a ServerRef field is NOT duplicate", func() {
-			By("Creating an BIOSVersion with different ServerRef")
-			biosVersionV2 := &metalv1alpha1.BIOSVersion{
-				ObjectMeta: metav1.ObjectMeta{
-					Namespace:    "ns.Name",
-					GenerateName: "test-",
-				},
-				Spec: metalv1alpha1.BIOSVersionSpec{
-					BIOSVersionTemplate: metalv1alpha1.BIOSVersionTemplate{
-						Version:                 "P71 v1.45 (12/06/2017)",
-						Image:                   metalv1alpha1.ImageSpec{URI: "asd"},
-						ServerMaintenancePolicy: metalv1alpha1.ServerMaintenancePolicyEnforced,
-					},
-					ServerRef: &v1.LocalObjectReference{Name: "bar"},
-				},
-			}
-			Expect(k8sClient.Create(ctx, biosVersionV2)).To(Succeed())
-			DeferCleanup(k8sClient.Delete, biosVersionV2)
-
-			By("Updating an biosVersionV2 to NON conflicting Spec.ServerRef ")
-			biosVersionV2Updated := biosVersionV2.DeepCopy()
-			biosVersionV2Updated.Spec.ServerRef = &v1.LocalObjectReference{Name: "foobar"}
-			Expect(validator.ValidateUpdate(ctx, biosVersionV2, biosVersionV2Updated)).Error().ToNot(HaveOccurred())
-		})
-
-		It("Should NOT allow update Version is in progress. but should allow to Force it", func() {
-			By("Patching the biosVersion V1 to InProgress state")
-			Eventually(UpdateStatus(biosVersionV1, func() {
-				biosVersionV1.Status.State = metalv1alpha1.BIOSVersionStateInProgress
-			})).Should(Succeed())
-			By("mock servermaintenance Creation maintenance")
-			Eventually(Update(biosVersionV1, func() {
-				biosVersionV1.Spec.ServerMaintenanceRef = &metalv1alpha1.ObjectReference{Name: "foobar-Maintenance"}
-			})).Should(Succeed())
-			By("Updating an biosVersion V1 spec, should fail to update when inProgress")
-			biosVersionV1Updated := biosVersionV1.DeepCopy()
-			biosVersionV1Updated.Spec.Version = "P712"
-			Expect(validator.ValidateUpdate(ctx, biosVersionV1, biosVersionV1Updated)).Error().To(HaveOccurred())
-			By("Updating an biosVersion V1 spec, should pass to update when inProgress with ForceUpdateResource finalizer")
-			biosVersionV1Updated.Annotations = map[string]string{metalv1alpha1.OperationAnnotation: metalv1alpha1.OperationAnnotationForceUpdateInProgress}
-			Expect(validator.ValidateUpdate(ctx, biosVersionV1, biosVersionV1Updated)).Error().ToNot(HaveOccurred())
-
-			Eventually(UpdateStatus(biosVersionV1, func() {
-				biosVersionV1.Status.State = metalv1alpha1.BIOSVersionStateCompleted
-			})).Should(Succeed())
-		})
-
-		It("Should refuse to delete if InProgress", func() {
-			By("Patching the biosVersionV1 to InProgress state")
-			Eventually(UpdateStatus(biosVersionV1, func() {
-				biosVersionV1.Status.State = metalv1alpha1.BIOSVersionStateInProgress
-			})).Should(Succeed())
-
-			By("Deleting the BIOSSettings V1 should fail")
-			Expect(k8sClient.Delete(ctx, biosVersionV1)).To(Not(Succeed()))
-
-			Eventually(UpdateStatus(biosVersionV1, func() {
-				biosVersionV1.Status.State = metalv1alpha1.BIOSVersionStateCompleted
-			})).Should(Succeed())
-
-			By("Deleting the BIOSSettings V1 should pass: by DeferCleanup")
-
-		})
+		By("Ensuring clean state")
+		controller.EnsureCleanState()
 	})
 
+	It("Should deny creation if a spec.serverRef field is duplicate", func(ctx SpecContext) {
+		By("Creating another BIOSVersion with existing ServerRef")
+		biosVersionV2 := &metalv1alpha1.BIOSVersion{
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "test-",
+			},
+			Spec: metalv1alpha1.BIOSVersionSpec{
+				BIOSVersionTemplate: metalv1alpha1.BIOSVersionTemplate{
+					Version:                 "P71 v1.45 (12/06/2017)",
+					Image:                   metalv1alpha1.ImageSpec{URI: "two"},
+					ServerMaintenancePolicy: metalv1alpha1.ServerMaintenancePolicyEnforced,
+				},
+				ServerRef: &v1.LocalObjectReference{Name: "foo"},
+			},
+		}
+		Expect(validator.ValidateCreate(ctx, biosVersionV2)).Error().To(HaveOccurred())
+	})
+
+	It("Should create if a spec.serverRef field is not a duplicate", func() {
+		By("Creating another BIOSVersion with different ServerRef")
+		biosVersionV2 := &metalv1alpha1.BIOSVersion{
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "test-",
+			},
+			Spec: metalv1alpha1.BIOSVersionSpec{
+				BIOSVersionTemplate: metalv1alpha1.BIOSVersionTemplate{
+					Version:                 "P71 v1.45 (12/06/2017)",
+					Image:                   metalv1alpha1.ImageSpec{URI: "asd"},
+					ServerMaintenancePolicy: metalv1alpha1.ServerMaintenancePolicyEnforced,
+				},
+				ServerRef: &v1.LocalObjectReference{Name: "bar"},
+			},
+		}
+		Expect(k8sClient.Create(ctx, biosVersionV2)).To(Succeed())
+	})
+
+	It("Should deny update if a spec.serverRef field is a duplicate", func() {
+		By("Creating an BIOSVersion with different ServerRef")
+		biosVersionV2 := &metalv1alpha1.BIOSVersion{
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "test-",
+			},
+			Spec: metalv1alpha1.BIOSVersionSpec{
+				BIOSVersionTemplate: metalv1alpha1.BIOSVersionTemplate{
+					Version:                 "P71 v1.45 (12/06/2017)",
+					Image:                   metalv1alpha1.ImageSpec{URI: "asd"},
+					ServerMaintenancePolicy: metalv1alpha1.ServerMaintenancePolicyEnforced,
+				},
+				ServerRef: &v1.LocalObjectReference{Name: "bar"},
+			},
+		}
+		Expect(k8sClient.Create(ctx, biosVersionV2)).To(Succeed())
+
+		By("Updating an BIOSVersion V2 to conflicting spec.serverRef")
+		biosVersionV2Updated := biosVersionV2.DeepCopy()
+		biosVersionV2Updated.Spec.ServerRef = &v1.LocalObjectReference{Name: "foo"}
+		Expect(validator.ValidateUpdate(ctx, biosVersionV2, biosVersionV2Updated)).Error().To(HaveOccurred())
+	})
+
+	It("Should allow update if a different field is duplicate", func() {
+		By("Creating an BIOSVersion with different ServerRef")
+		biosVersionV2 := &metalv1alpha1.BIOSVersion{
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "test-",
+			},
+			Spec: metalv1alpha1.BIOSVersionSpec{
+				BIOSVersionTemplate: metalv1alpha1.BIOSVersionTemplate{
+					Version:                 "P71 v1.45 (12/06/2017)",
+					Image:                   metalv1alpha1.ImageSpec{URI: "two"},
+					ServerMaintenancePolicy: metalv1alpha1.ServerMaintenancePolicyEnforced,
+				},
+				ServerRef: &v1.LocalObjectReference{Name: "bar"},
+			},
+		}
+		Expect(k8sClient.Create(ctx, biosVersionV2)).To(Succeed())
+
+		By("Updating an BIOSVersion V2 to conflicting spec.biosVersionSpec")
+		biosVersionV2Updated := biosVersionV2.DeepCopy()
+		biosVersionV2Updated.Spec.Image = biosVersionV1.Spec.Image
+		Expect(validator.ValidateUpdate(ctx, biosVersionV2, biosVersionV2Updated)).Error().ToNot(HaveOccurred())
+	})
+
+	It("Should allow update if a ServerRef field is not a duplicate", func() {
+		By("Creating an BIOSVersion with different ServerRef")
+		biosVersionV2 := &metalv1alpha1.BIOSVersion{
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "test-",
+			},
+			Spec: metalv1alpha1.BIOSVersionSpec{
+				BIOSVersionTemplate: metalv1alpha1.BIOSVersionTemplate{
+					Version:                 "P71 v1.45 (12/06/2017)",
+					Image:                   metalv1alpha1.ImageSpec{URI: "asd"},
+					ServerMaintenancePolicy: metalv1alpha1.ServerMaintenancePolicyEnforced,
+				},
+				ServerRef: &v1.LocalObjectReference{Name: "bar"},
+			},
+		}
+		Expect(k8sClient.Create(ctx, biosVersionV2)).To(Succeed())
+
+		By("Updating an BIOSVersion V2 to a non-conflicting spec.serverRef ")
+		biosVersionV2Updated := biosVersionV2.DeepCopy()
+		biosVersionV2Updated.Spec.ServerRef = &v1.LocalObjectReference{Name: "foobar"}
+		Expect(validator.ValidateUpdate(ctx, biosVersionV2, biosVersionV2Updated)).Error().ToNot(HaveOccurred())
+	})
+
+	It("Should no allow update a BIOSVersion is in progress, but should allow to force update it", func() {
+		By("Patching the BIOSVersion V1 to in-progress state")
+		Eventually(UpdateStatus(biosVersionV1, func() {
+			biosVersionV1.Status.State = metalv1alpha1.BIOSVersionStateInProgress
+		})).Should(Succeed())
+
+		By("Add ServerMaintenance reference")
+		Eventually(Update(biosVersionV1, func() {
+			biosVersionV1.Spec.ServerMaintenanceRef = &metalv1alpha1.ObjectReference{Name: "maintenance"}
+		})).Should(Succeed())
+
+		By("Updating the BIOSVersion V1 spec should fail to update when in InProgress state")
+		biosVersionV1Updated := biosVersionV1.DeepCopy()
+		biosVersionV1Updated.Spec.Version = "P712"
+		Expect(validator.ValidateUpdate(ctx, biosVersionV1, biosVersionV1Updated)).Error().To(HaveOccurred())
+
+		By("Updating an biosVersion V1 spec, should pass to update when inProgress with ForceUpdateResource finalizer")
+		biosVersionV1Updated.Annotations = map[string]string{metalv1alpha1.OperationAnnotation: metalv1alpha1.OperationAnnotationForceUpdateInProgress}
+		Expect(validator.ValidateUpdate(ctx, biosVersionV1, biosVersionV1Updated)).Error().ToNot(HaveOccurred())
+
+		By("Patching the BIOSVersion V1 to Completed state")
+		Eventually(UpdateStatus(biosVersionV1, func() {
+			biosVersionV1.Status.State = metalv1alpha1.BIOSVersionStateCompleted
+		})).Should(Succeed())
+	})
+
+	It("Should refuse to delete if InProgress", func() {
+		By("Patching the BIOSVersionV1 to InProgress state")
+		Eventually(UpdateStatus(biosVersionV1, func() {
+			biosVersionV1.Status.State = metalv1alpha1.BIOSVersionStateInProgress
+		})).Should(Succeed())
+
+		By("Deleting the BIOSSettings V1 should fail")
+		Expect(k8sClient.Delete(ctx, biosVersionV1)).To(Not(Succeed()))
+
+		Eventually(UpdateStatus(biosVersionV1, func() {
+			biosVersionV1.Status.State = metalv1alpha1.BIOSVersionStateCompleted
+		})).Should(Succeed())
+	})
 })

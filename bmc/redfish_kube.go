fefb6861ee47915d6fe32c8e6e6496e716cbb1d1
Andreas Fritzler <andreas.fritzler@sap.com>
2026-01-14 12:54:04 +0100

    Fix ctx handling in kube redfish client

--- bmc/redfish_kube.go
+++ bmc/redfish_kube.go
@@ -396,7 +396,7 @@ func (r *RedfishKubeBMC) SetPXEBootOnce(ctx context.Context, systemURI string) e
 		"-c",
 		curlCmd,
 	}
-	if err := r.createJob(context.TODO(), r.kclient, cmd, r.namespace, system.UUID); err != nil {
+	if err := r.createJob(ctx, r.kclient, cmd, r.namespace, system.UUID); err != nil {
 		return fmt.Errorf("failed to create job for system %s: %w", system.UUID, err)
 	}
 	return nil

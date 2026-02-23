bed5ceadfe82259e1332f46344cc92fdf0ca842a
Andreas Fritzler <andreas.fritzler@sap.com>
2026-02-23 10:17:02 +0100

    Increase BMC reset time interval

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -183,7 +183,7 @@ func SetupTest(redfishMockServers []netip.AddrPort) *corev1.Namespace {
 			Scheme:                 k8sManager.GetScheme(),
 			Insecure:               true,
 			ManagerNamespace:       ns.Name,
-			BMCResetWaitTime:       100 * time.Millisecond,
+			BMCResetWaitTime:       150 * time.Millisecond,
 			BMCClientRetryInterval: 25 * time.Millisecond,
 			EventURL:               "http://localhost:8008",
 			DNSRecordTemplate:      dnsTemplate,
